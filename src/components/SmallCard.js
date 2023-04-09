ec2d5a060f539b8a233b2f2a50f36cfd6b8be45b
Simon Hoefling <simon@hoeflia.eu>
2023-04-09 16:07:35 +0800

    Added to get some data from an API

---
+++ src/components/SmallCard.js
@@ -0,0 +1,9 @@
+function SmallCard({img, location, distance}) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SmallCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Banner from '@/components/Banner'
+import SmallCard from '@/components/SmallCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className=''>
       <Head>
@@ -14,6 +15,35 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull some date from a server - API Endpoints */}
+          {exploreData?.map(({img, distance, location}) => (
+            <SmallCard
+              key={img}
+              img={img}
+              distance={distance}
+              location={location}
+            />
+          ))}
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
